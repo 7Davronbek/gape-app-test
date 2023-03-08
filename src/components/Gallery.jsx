ab8c6844cceac81a1595228a12428662104dc9a6
7Davronbek <maxmudovdavronbek8@gmail.com>
2023-03-08 14:15:41 +0300

    gape-app

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -11,15 +11,17 @@ const Gallery = () => {
         </div>
 
         <div className="row">
-          <div className="col-lg-8 mb-4">
+          <div className="col-lg-8 mb-4 topCol">
+            <h2>“Jorem ipsum dolor sit amet, consectetur adipiscing elit.”</h2>
             <img className="w-100"  src="/assets/images/gallary1.png" alt="" />
           </div>
 
-          <div className="col-lg-4 mb-4">
+          <div className="col-lg-4 mb-4 ">
             <img  className="w-100" src="/assets/images/gallary2.png" alt="" />
           </div>
 
-          <div className="col-lg-7 mb-4">
+          <div className="col-lg-7 mb-4 topCol">
+            <h3>“Jorem ipsum dolor sit amet, consectetur adipiscing elit.”</h3>
             <img src="/assets/images/gallary3.png" alt="" className="w-100" />
           </div>
           
