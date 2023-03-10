8dda8eeef362a22bd39d7d6d9664f1d1fe563e5a
7Davronbek <maxmudovdavronbek8@gmail.com>
2023-03-10 10:21:33 +0300

    gape-app

--- src/pages/Calendar.jsx
+++ src/pages/Calendar.jsx
@@ -1,16 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Calendar = () => {
+  const [filter, setFilter] = useState(false);
   return (
     <>
-      <div className="Calendar">
+      <div onClick={() => setFilter(false)} className="Calendar">
         <div className="container">
           <div className="row">
             <div className="col-lg-11 mx-auto">
               <h1>Календарь</h1>
 
               <div className="d-flex align-items-center justify-content-between ">
-                <div className="filter">
+                <div
+                  onClick={(e) => {
+                    setFilter(true), e.stopPropagation();
+                  }}
+                  className="filter"
+                >
                   <span>
                     <img src="/assets/icons/filter.svg" alt="" />
                   </span>
@@ -140,20 +146,92 @@ const Calendar = () => {
         </div>
       </div>
 
-      <div className="Filter">
+      <div className={`Filter ${filter ? "active" : ""}`}>
         <div className="container">
           <div className="row">
-            <div className="col-lg-4">
-              <div className="filter">
-                <span>
-                  <img src="/assets/icons/filter.svg" alt="" />
-                </span>
-                <h6>фильтр</h6>
-              </div>
+            <div>
+              <div onClick={(e) => e.stopPropagation()} className="cards">
+                <div className="close">
+                  <img
+                    onClick={() => setFilter(false)}
+                    src="/assets/icons/close.svg"
+                    alt=""
+                  />
+                </div>
+                <div className="filter">
+                  <span>
+                    <img src="/assets/icons/filter.svg" alt="" />
+                  </span>
+                  <h6>фильтр</h6>
+                </div>
 
-              <div className="inputWrap">
-                <input type="checkbox" id="lorem" />
-                <label htmlFor="lorem">Worem ipsum </label>
+                <div className="inputWrap">
+                  <input type="checkbox" id="12" />
+                  <label htmlFor="12">Worem ipsum </label>
+                </div>
+
+                <div className="inputWrap">
+                  <input type="checkbox" id="23" />
+                  <label htmlFor="23">Worem ipsum </label>
+                </div>
+
+                <div className="inputWrap">
+                  <input type="checkbox" id="34" />
+                  <label htmlFor="34">Worem ipsum </label>
+                </div>
+
+                <div className="inputWrap">
+                  <input type="checkbox" id="45" />
+                  <label htmlFor="45">Worem ipsum </label>
+                </div>
+
+                <div className="inputWrap">
+                  <input type="checkbox" id="56" />
+                  <label htmlFor="56">Worem ipsum </label>
+                </div>
+
+                <div className="filter">
+                  <h6>время</h6>
+                </div>
+
+                <input type="time" className="form-control" />
+
+                <div className="filter">
+                  <h6>Место</h6>
+                </div>
+
+                <div className="inputWrap">
+                  <input type="checkbox" id="lorem" />
+                  <label htmlFor="lorem">Worem ipsum </label>
+                </div>
+
+                <div className="inputWrap">
+                  <input type="checkbox" id="lorem2" />
+                  <label htmlFor="lorem2">Worem ipsum </label>
+                </div>
+
+                <div className="inputWrap">
+                  <input type="checkbox" id="lorem3" />
+                  <label htmlFor="lorem3">Worem ipsum </label>
+                </div>
+
+                <div className="inputWrap">
+                  <input type="checkbox" id="lore4" />
+                  <label htmlFor="lore4">Worem ipsum </label>
+                </div>
+
+                <div className="inputWrap">
+                  <input type="checkbox" id="5" />
+                  <label htmlFor="5">Worem ipsum </label>
+                </div>
+
+                <div className="filter">
+                  <h6>день</h6>
+                </div>
+
+                <input type="date" className="form-control" />
+
+                <button className="btn">готово</button>
               </div>
             </div>
           </div>
